1a26615afbaace8c514d40ab75c91480624a0a70
catalin.butacu <catalin.butacu@geminicad.com>
2024-04-08 00:12:32 +0300

    Adding Master-Slave Architecture

--- MS.c
+++ MS.c
@@ -12,7 +12,7 @@
 nod retea[NR_NODURI];					// retea cu 5 noduri
 
 unsigned char STARE_COM = 0;		// starea initiala a FSA COM
-unsigned char TIP_NOD	= 0;		// tip nod initial: Slave sau No JET
+unsigned char TIP_NOD	= 0;		  // tip nod initial: Slave sau No JET
 unsigned char STARE_IO 	= 0;		// stare interfata IO - asteptare comenzi
 unsigned char ADR_MASTER;				// adresa nod master - numai MS
 
@@ -24,253 +24,220 @@ unsigned char RxMesaj(unsigned char i);		// primire mesaj de la nodul i
 
 //***********************************************************************************************************
 void main (void) {
-	unsigned char i, found;	// variabile locale
+	// variabile locale
+	unsigned char i, found;	
 	
-	WDT_Disable();												// dezactiveaza WDT
-	SYSCLK_Init();											// initializeaza si selecteaza oscilatorul ales in osc.h
-	UART1_Init(NINE_BIT, BAUDRATE_COM);		// initilizare UART1 - port comunicatie (TxD la P1.0 si RxD la P1.1)
-	UART1_TxRxEN (1,1);									// validare Tx si Rx UART1
+	// dezactiveaza WDT
+	WDT_Disable();	
+	// initializeaza si selecteaza oscilatorul ales in osc.h
+	SYSCLK_Init();	
+	// initilizare UART1 - port comunicatie (TxD la P1.0 si RxD la P1.1)
+	UART1_Init(NINE_BIT, BAUDRATE_COM);	
+	// validare Tx si Rx UART1
+	UART1_TxRxEN (1,1);	
+	// conecteaza perifericele la pini (UART0, UART1) si stabileste tipul pinilor
+ 	PORT_Init ();	
+
+	// initilizeaza LCD-ul
+	LCD_Init();    							
+	// initializare driver tastatura matriciala locala					
+	KEYB_Init();										
+	// initializare UART0  - conectata la USB-UART (P0.0 si P0.1)			
+	UART0_Init(EIGHT_BIT, BAUDRATE_IO);		
+
+	// initializare Timer 0
+	Timer0_Init();  								
+
+	// valideaza intreruperile
+ 	EA = 1;                         	
+	// selecteaza pagina 0 SFR		
+ 	SFRPAGE = LEGACY_PAGE;          			
 	
- 	PORT_Init ();													// conecteaza perifericele la pini (UART0, UART1) si stabileste tipul pinilor
-
-	LCD_Init();    												// 2 linii, display ON, cursor OFF, pozitia initiala (0,0)
-	KEYB_Init();													// initializare driver tastatura matriciala locala
-	UART0_Init(EIGHT_BIT, BAUDRATE_IO);		// initializare UART0  - conectata la USB-UART (P0.0 si P0.1)
-
-	Timer0_Init();  								// initializare Timer 0
-
- 	EA = 1;                         			// valideaza intreruperile
- 	SFRPAGE = LEGACY_PAGE;          			// selecteaza pagina 0 SFR
-	
-	for(i = 0; i < NR_NODURI; i++){		// initializare structuri de date
-		retea[i].full = 0;						// initializeaza buffer gol pentru toate nodurile
-		retea[i].bufasc[0] = ':';				// pune primul caracter in buffer-ele ASCII ":"
+	// initializare structuri de date
+	for(i = 0; i < NR_NODURI; i++)
+	{		
+		// initializeaza buffer gol pentru toate nodurile
+		retea[i].full = 0;		
+		// pune primul caracter in buffer-ele ASCII ":"				
+		retea[i].bufasc[0] = ':';				
 	}
 
-	Afisare_meniu();			   				// Afiseaza meniul de comenzi
+	// Afiseaza meniul de comenzi
+	Afisare_meniu();			   				
 	
- 	while(1){												// bucla infinita
-																
-		switch(STARE_COM){
-			case 0:
-
-#if(PROTOCOL == MS)									// nodul este slave, asteapta mesaj complet si corect de la master	
-
-				switch(RxMesaj(ADR_NOD)){				// asteapta un mesaj de la master
-					case TMO:								// anunta ca nodul curent devine master
-																	// nodul curent devine master
-																	// Afiseaza meniul de comenzi
-																	// trece in starea 2
-																	// primul slave va fi cel care urmeaza dupa noul master
-									break;
-
-					case ROK:									break;	// a primit un mesaj de la master, il afiseaza si trebuie sa raspunda
-					case POK:	STARE_COM = 1; 	break;		
-					case CAN:									break;	// afiseaza eroare Mesaj incomplet
-					case TIP:									break;	// afiseaza eroare Tip mesaj necunoscut
-					case ESC:									break;	// afiseaza Eroare SC
-					default:									break;	// afiseaza cod eroare necunoscut, asteapta 1 sec
-				}
-				break;
-#endif
-
-#if(PROTOCOL == JT)	// nodul nu detine jetonul, asteapta un mesaj util sau jetonul
-				
-				switch(RxMesaj(ADR_NOD)){					// asteapta jetonul de la master
-					case TMO:
-																					// anunta ca nodul a regenerat jetonul 
-																					// nodul curent detine jetonul
-																					// Daca e permisa afisarea, afiseaza meniul de comenzi
-																					// trece in starea 1
+ 	while(1) // bucla infinita
+	{										
+		switch(STARE_COM)
+		{
+			// nodul este slave, asteapta mesaj complet si corect de la master	
+			case 0:		
+				// asteapta un mesaj de la master
+				switch(RxMesaj(ADR_NOD))
+				{							
+					case TMO:	
+						// anunta ca nodul curent devine master
+						Error("\n\rNod SLAVE -> MASTER!");  
+						// nodul curent devine master
+						TIP_NOD = MASTER;				
+						// Afiseaza meniul de comenzi						
+						Afisare_meniu(); 
+						// trece in starea 2										
+						STARE_NOD = 2;	
+						// primul slave va fi cel care urmeaza dupa noul master										
+						i = ADR_NOD;												
 						break;
 
-					case ROK:										break;						// a primit un mesaj USER_MES, il afiseaza
-					case JOK:										// a primit un jetonul
-						
-						Delay(WAIT/2);						// asteapta WAIT/2 ms
+					case ROK:		// a primit un mesaj de la master, il afiseaza si trebuie sa raspunda
+						STARE_NOD = 1; 
+						Afisare_mesaj(); 
+						break;	
 					
-																						// adresa HW dest este adr_hw_src
-																						// adresa HW src este ADR_NOD
-																						// tip mesaj = JET_MES
-																						// transmite mesajul JET_MES din bufferul ADR_NOD
-																						// nodul curent detine jetonul
-						if(AFISARE) 										// Daca e permisa afisarea, afiseaza meniul de comenzi		
-																						// trece in starea 1
-						break; // nodul detine jetonul, poate trece sa transmita un mesaj de date
-
-					case CAN:												break;		// afiseaza eroare Mesaj incomplet
-					case CAN_adresa_hw_src:	 				break;		// afiseaza eroare Mesaj incomplet (adresa_hw_src)
-					case CAN_tipmes:								break;		// afiseaza eroare Mesaj incomplet (tip mesaj)
-					case CAN_sc:										break;		// afiseaza eroare Mesaj incomplet (sc)
-					case TIP:												break;		// afiseaza eroare Tip mesaj necunoscut
-					case ESC:	 											break;				// afiseaza Eroare SC
-					default:												break;			// afiseaza cod eroare necunoscut, apoi asteapta 1 sec
+					case POK:	
+						STARE_COM = 1; 	
+						break;		
+					
+					case CAN:	
+						Error("\n\rMesaj incomplet!"); // afiseaza eroare Mesaj incomplet
+						break;
+					
+					case TIP:	
+						Error("\n\rTip mesaj necunoscut!"); // afiseaza eroare Tip mesaj necunoscut
+						break;	
+					
+					case ESC:									
+						Error("\n\rEroare SC!"); // afiseaza Eroare SC
+						break;	
+					
+					default:	
+						Error("\n\rEroare necunoscuta!"); // afiseaza cod eroare necunoscut, asteapta 1 sec
+						break;	
 				}
 				break;
-#endif								
 
-			case 1:											
+			// nodul este slave, transmite mesaj catre master	
+			case 1:				
+				// cauta sa gaseasca un mesaj util de transmis
+				found = 0;  
+				for(i = 0; i < NR_NODURI; i++){
+					if(retea[i].full == 1){
+							found = 1;
+							break;
+					}
+				}																
+				
+				if(found) // daca gaseste un nod i
+				{  
+					// pune adresa HW dest este ADR_MASTER
+					retea[i].bufbin.adresa_hw_dest = ADR_MASTER; 
+					// transmite mesajul catre nodul i
+					TxMesaj(i); 
+				}
+				else // daca nu gaseste, construieste un mesaj in bufferul ADR_MASTER
+				{  
+					// adresa HW dest este ADR_MASTER
+					retea[ADR_MASTER].bufbin.adresa_hw_dest = ADR_MASTER;
+					// adresa HW src este ADR_NOD 
+					retea[ADR_MASTER].bufbin.adresa_hw_src = ADR_NOD;	
+					// tip mesaj = POLL_MES
+					retea[ADR_MASTER].bufbin.tipmes = POLL_MES;		
+					// transmite mesajul din bufferul ADR_MASTER
+					TxMesaj(ADR_MASTER);  
+				}
 
-#if(PROTOCOL == MS)										// nodul este slave, transmite mesaj catre master			
-																
-																				// cauta sa gaseasca un mesaj util de transmis
-												
-																
-																
-															
-														
-																		// daca gaseste un nod i
-																			// pune adresa HW dest este ADR_MASTER
-																			// transmite mesajul catre nodul i
-																
-																		// daca nu gaseste, construieste un mesaj in bufferul ADR_MASTER
-																			// adresa HW dest este ADR_MASTER
-																			// adresa HW src este ADR_NOD
-																			// tip mesaj = POLL_MES
-																			// transmite mesajul din bufferul ADR_MASTER
-															
-																// trece in starea 0, sa astepte un nou mesaj de la master
+				// trece in starea 0, sa astepte un nou mesaj de la master
+				STARE_NOD = 0;				
 				break;
-#endif
 
-#if(PROTOCOL == JT)							// nodul detine jetonul, poate transmite un mesaj USER_MES				
-				
-				
-																			// cauta sa gaseasca un mesaj util de transmis
-	
+			// tratare stare 2 si comutare stare	
+			case 2:		
+				// nodul este master, tratare stare 2 si comutare stare
+				do {
+					// selecteaza urmatorul slave (incrementeaza i)
+					i = (++i) % NR_NODURI;	
+				} while(i == ADR_NOD);
 
+				// adresa HW dest este i	
+				retea[i].bufbin.adresa_hw_dest = i;	
 
+				// daca in bufferul i se afla un mesaj util, il transmite										
+				if(retea[i].full == 1)  
+				{
+					TxMesaj(i);									
+				}
+				else  // altfel, construieste un mesaj de interogare in bufferul i
+				{	
+					// adresa HW src este ADR_NOD
+					retea[i].bufbin.adresa_hw_src = ADR_NOD; 
+					// tip mesaj = POLL_MES
+					retea[i].bufbin.tipmes = POLL_MES;      
+					// transmite mesajul din bufferul i
+					TxMesaj(i);
+				}				
 
+				// trece in starea 3, sa astepte raspunsul de la slave-ul i						
+				STARE_NOD = 3; 
+				break;
 
-																					// daca gaseste un mesaj de transmis catre nodul i
-																						// adresa HW dest este dest
-																						// transmite mesajul catre nodul i
-																						// asteapta procesarea mesajului la destinatie (WAIT/2 msec)
+			// nodul este master, asteapta mesaj de la slave
+			case 3:			
+				// nodul este slave, asteapta mesaj de la master	
+				switch(RxMesaj(i)) // asteapta un raspuns de la slave i
+				{						
+					case TMO:	
+						// afiseaza eroare Timeout nod i
+						Error("\n\rTimeout nod ");
+						UART0_Putch(i+'0');
+						break;
 
-																		// va incerca sa transmita jetonul nodului urmator 
-																		// trece in starea 2, sa transmita jetonul urmatorului nod
-#endif	
-				
-			break;	
-				
-			case 2:											// tratare stare 2 si comutare stare
-
-#if(PROTOCOL == MS)											// nodul este master, tratare stare 2 si comutare stare
-															
-																	// selecteaza urmatorul slave (incrementeaza i)
-																	
-														
-														
-	
-																	// adresa HW dest este i
-																	// daca in bufferul i se afla un mesaj util, il transmite
-																	// altfel, construieste un mesaj de interogare in bufferul i
-																	// adresa HW src este ADR_NOD
-																	// tip mesaj = POLL_MES
-																	// transmite mesajul din bufferul i
-															
-																// trece in starea 3, sa astepte raspunsul de la slave-ul i
-
-#endif
-
-#if(PROTOCOL == JT)											// nodul transmite jetonul urmatorului nod 		
-				
-																				// selecteaza urmatorul slave (incrementeaza i)
-					
-	
-	
-	
-				Delay(WAIT/2);									// asteapta WAIT/2 sec
-				
-																				// adresa HW dest este i
-																				// adresa HW src este ADR_NOD
-																				// tip mesaj = JET_MES
-																				// transmite mesajul din bufferul i
-																				// trece in starea 3, sa astepte confirmarea de la nodul i ca jetonul a fost primit
-#endif
-
-			break;
-
-			case 3:
-
-#if(PROTOCOL == MS)										// nodul este master, asteapta mesaj de la slave	
-
-			// nodul este slave, asteapta mesaj de la master	
-				switch(RxMesaj(i)){						// asteapta un raspuns de la slave i
-					case TMO:													// afiseaza eroare Timeout nod i
-								
-									break;
-					case ROK:													break;	// a primit un mesaj, il afiseaza
+					case ROK:	break;	// a primit un mesaj, il afiseaza
 					case POK:	break;							
 
-					case ERI:													break;	// afiseaza Eroare incadrare
-					case ERA:													break;	// afiseaza Eroare adresa
-					case TIP:													break;	// afiseaza Tip mesaj necunoscut
-					case OVR:													break;	// afiseaza Eroare suprapunere
-					case ESC:													break;	// afiseaza Eroare SC
-					case CAN:													break;	// afiseaza mesaj incomplet
+					case ERI:	
+						// afiseaza Eroare incadrare
+						Error("\n\rEroare incadrare!");
+						break;	
 
-					case TEST:											 break;					// afiseaza Eroare TEST
-					default:														break;	// afiseaza Eroare necunoscuta, apoi asteapta 1000ms
+					case ERA: 
+						// afiseaza Eroare adresa
+						Error("\n\rEroare adresa!");
+						break;	
+
+					case TIP:
+						// afiseaza Tip mesaj necunoscut
+						Error("\n\rTip mesaj necunoscut!");
+						break;	
+
+					case OVR:	
+						// afiseaza Eroare suprapunere
+						Error("\n\rEroare suprapunere!");
+						break;	
+
+					case ESC:
+						// afiseaza Eroare SC
+						Error("\n\rEroare SC!")
+						break;	
+
+					case CAN:
+						// afiseaza mesaj incomplet
+						Error("\n\rMesaj incomplet!")
+						break;	
+
+					case TEST:
+						// afiseaza Eroare TEST
+						Error("\n\rMesaj incomplet!")	
+						break;				
+
+					default:
+						// afiseaza Eroare necunoscuta, apoi asteapta 1000ms
+						Error("\n\rEroare necunoscuta!");
+						break;	
 				}
-																// revine in starea 2 (a primit sau nu un raspuns de la slave, trece la urmatorul slave)
-
-#endif
-
-			
-#if(PROTOCOL == JT)					
-				switch(RxMesaj(ADR_NOD)){									// asteapta un raspuns de la nod i
-					case TMO:	Error("\n\rTimeout nod ");		// afiseaza eroare Timeout nod i
-								
-
-																									// revine in starea 2 (nu a primit raspuns)
-								break;
-					case JOK:																// a primit confirmarea transferului jetonului, revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// daca afisarea e permisa, afiseaza meniul
-								break;
-					case ERI:																// afiseaza Eroare incadrare
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					case ERA:																// afiseaza Eroare adresa
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case CAN:																// afiseaza Mesaj incomplet
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case TIP:																// afiseaza Tip mesaj necunoscut
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-					
-					case ESC:																// afiseaza Eroare SC
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-
-					default:																// afiseaza Eroare necunoscuta
-																									// asteapta 1000 ms
-																									// revine in starea 0
-																									// nodul nu mai detine jetonul
-																									// afiseaza meniul
-										break;			
-			}
-				
-#endif
-			
-			break;			
+				// revine in starea 2 (a primit sau nu un raspuns de la slave, trece la urmatorul slave)
+				STARE_NOD = 2;
+				break;			
 		}
 		
-		UserIO();							// apel functie interfata
+		// apel functie interfata
+		UserIO();							
 	}
 }
